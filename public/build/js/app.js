a424d7f6bf163dbee11a54db7d4d281f534a0940
Maurilio Silva <mauriliodeveloper@gmail.com>
2016-04-19 10:24:38 -0300

    Erro Função Login Js

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,13 +1,35 @@
-var app = angular.module('app', ['ng-route', 'app.controllers']);
+var app = angular.module('app', ['ngRoute', 'angular-oauth2', 'app.controllers']);
 
-angular.module('app.controllers', []);
-app.config(function($routeProvider){
+angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
+app.config(['$routeProvider', 'OAuthProvider', function($routeProvider, OAuthProvider){
     $routeProvider.when('/login', {
-       templateUrl: 'views/login.html',
-       Controller: 'LoginController'
-    }).
-    $routeProvider.when('/home', {
+       templateUrl: 'build/views/login.html',
+       controller: 'LoginController'
+    }).when('/home', {
        templateUrl: 'build/views/home.html',
-       Controller: 'HomeController'
+       controller: 'HomeController'
+     
+     });
+      OAuthProvider.configure({
+      baseUrl: 'https://localhost/PlanoDeAcao/public',
+      clientId: 'app',
+      clientSecret: 'secret', // optional
+      grantPath: 'oauth/access_token'
+  });
+}]);
+ app.run(['$rootScope', '$window', 'OAuth', function($rootScope, $window, OAuth) {
+    $rootScope.$on('oauth:error', function(event, rejection) {
+      // Ignore `invalid_grant` error - should be catched on `LoginController`.
+      if ('invalid_grant' === rejection.data.error) {
+        return;
+      }
+
+      // Refresh token when a `invalid_token` error occurs.
+      if ('invalid_token' === rejection.data.error) {
+        return OAuth.getRefreshToken();
+      }
+
+      // Redirect to `/login` with the `error_reason`.
+      return $window.location.href = '/login?error_reason=' + rejection.data.error;
     });
-});
+ }]);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
         @if(Config::get('app.debug'))
-        <link href="{{asset('build/css/vendor/bootstrap.min.css')}}" rel="stylesheet"/>
-        <link href="{{asset('build/css/vendor/bootstrap-theme.min.css')}}" rel="stylesheet"/>
+        /<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">        
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <link href="{{asset('build/css/estilo.css')}}" rel="stylesheet"/>
+        <!-- Optional theme -->
+
         @else
         <link href="{{elixir('css/all.css')}}" rel="stylesheet"/>
         @endif
@@ -21,9 +27,19 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+        <style>
+            body{
+                background-image: url('build/img/tecnologia.jpg');
+                background-repeat: no-repeat;
+                background-size: 100% 100%;
+                
+                
+            }
+        </style>
 </head>
 <body>
-	<nav class="navbar navbar-default">
+<!--
+    <nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
@@ -60,8 +76,8 @@
 			</div>
 		</div>
 	</nav>
-
-    <div ng-view=""></div>
+-->
+    <div ng-view></div>
 
 	<!-- Scripts -->
 	@if(Config::get('app.debug'))
@@ -73,7 +89,12 @@
             <script src="{{asset('build/js/vendor/angular-messages.min.js')}}"></script>
             <script src="{{asset('build/js/vendor/ui-bootstrap.min.js')}}"></script>
             <script src="{{asset('build/js/vendor/navbar.min.js')}}"></script>
+            <script src="{{asset('build/js/vendor/angular-cookies.min.js')}}"></script>
+            <script src="{{asset('build/js/vendor/query-string.js')}}"></script>
+            <script src="{{asset('build/js/vendor/angular-oauth2.min.js')}}"></script>
             <script src="{{asset('build/js/app.js')}}"></script>
+            <script src="{{asset('build/js/controllers/home.js')}}"></script>
+            <script src="{{asset('build/js/controllers/login.js')}}"></script>
         @else
             <script src="{{elixir('js/all.js')}}"></script>
         @endif
